bfbd50b7bc51417f27bdbe90e25b9a6644017f6f
EC2 Default User <ec2-user@ip-172-31-42-103.ap-northeast-1.compute.internal>
2019-06-03 08:26:28 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#Task.create(content: 'test content1',status:'未完了')
+#Task.create(content: 'test content2',status:'完了')
+#Task.create(content: 'test content3',status:'未完了')
+
+(1..100).each do |number|
+    Task.create(content: 'test content'+number.to_s,status:'未完了')
+end
