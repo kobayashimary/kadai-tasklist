52c1c8c75c3638dd41ade18768a2739f9efaab1d
EC2 Default User <ec2-user@ip-172-31-42-103.ap-northeast-1.compute.internal>
2019-06-05 08:40:47 +0000

    kadai saiteishutu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-before_action :require_user_logged_in, only: [:index, :show]
+#before_action :require_user_logged_in, only: [:index, :show]
+=begin
   def index
     @users=User.order(id: :desc).page(params[:id]).per(25)
   end
@@ -7,7 +8,7 @@ before_action :require_user_logged_in, only: [:index, :show]
   def show
     @user=User.find(params[:id])
   end
-  
+=end  
   def new
     @user=User.new
   end
